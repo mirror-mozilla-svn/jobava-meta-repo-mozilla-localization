33c59b78296ef329dde6a35073ce1f2d4cf5d178
Jobava <jobaval10n@gmail.com>
2015-07-27 12:20:18 +0300

    latest changes + 'google-play' replaced by 'appstores', which is now hosted on github instead of svn

--- init.sh
+++ init.sh
@@ -7,12 +7,14 @@ while read reponame repourl
 do
     if [[ -n "$reponame" && -n "$repourl" && $reponame != "reponame" && $repourl != "repourl" ]]; then
         echo "Creating $reponame ..."
+        rm -rf "$reponame" 
         mkdir "$reponame"
         svn checkout "$repourl" "$reponame"
     fi
 done < $INPUT
 IFS=$OLDIFS
-
+rm -rf appstores
+git clone https://github.com/mozilla-l10n/appstores appstores
 echo "*~" > .gitignore
 echo ".svn" >> .gitignore
 echo ".git" >> .gitignore
