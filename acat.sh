7f770c493dd66c9ae41b4e979846512500cdd180
Jobava <jobaval10n@gmail.com>
2015-08-19 16:30:19 +0300

    added acat.sh, a better 'cat'

---
+++ acat.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Extract only the translated strings from a .lang file, line by line
+
+if [ -z "$1" ]; then
+    echo "Print lines in a file, printing the filename and line number for each line"
+    exit 1
+fi
+
+filename="$1"
+line_number=0
+while IFS='' read -r line || [[ -n $line ]]; do
+    line_number=$((line_number+1))
+    echo "$filename:$line_number|$line"
+done < "$filename"
+
